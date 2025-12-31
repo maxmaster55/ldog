92854e924aa3bc05bffc3384e26c92ef78cd8573
maxmaster <yousseflkashef000@gmail.com>
2025-12-31 23:58:24 +0200

    added moved path

--- app/main.cc
+++ app/main.cc
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include "ConsoleSinkImpl.h"
 
 int main(int argc, char const *argv[])
 {
+    ConsoleSinkImpl d;
+    d.write("test");
     std::cout<<"Hello World\n";
     return 0;
 }

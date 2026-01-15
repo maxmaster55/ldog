1a5f6d82bf81334f931938f936447ee10bbc9d06
maxmaster <yousseflkashef000@gmail.com>
2026-01-15 16:19:46 +0200

    refactor LogMessage to simplify time handling and remove unused functions

--- src/LogMessage.cc
+++ src/LogMessage.cc
@@ -1,21 +1,12 @@
 #include "LogMessage.h"
-#include <chrono>
-#include <ctime>
-#include <iomanip>
+
 #include <iostream>
 
 
-std::string tp_to_string(const std::chrono::system_clock::time_point& tp) {
-    std::time_t t = std::chrono::system_clock::to_time_t(tp);
-    std::tm tm = *std::localtime(&t); // convert to local time
-    std::stringstream ss;
-    ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
-    return ss.str();
-}
 
 std::ostream& operator<<(std::ostream& os, const LogMessage& msg) {
     os <<"[" << msg.app_name << "] "
-    <<"[" << tp_to_string(msg.time) << "] " 
+    <<"[" << msg.time << "] " 
     <<"[" << msg.context << "] "
     <<"[" << magic_enum::enum_name(msg.level) << "] "
     <<"[" << msg.text << "]"
@@ -25,7 +16,7 @@ std::ostream& operator<<(std::ostream& os, const LogMessage& msg) {
 
 
 
-LogMessage::LogMessage(string app_name, string context, time_point time, SeverityLvl level, string text)
+LogMessage::LogMessage(string app_name, string context, string time, SeverityLvl level, string text)
 :app_name(app_name), context(context), time(time), level(level), text(text)
 {
     

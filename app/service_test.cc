016a0c119329690947339c097b71dc8b5af90f71
maxmaster <yousseflkashef000@gmail.com>
2026-02-16 21:15:43 +0200

    tempreture sensor reading service

--- app/service_test.cc
+++ app/service_test.cc
@@ -2,27 +2,44 @@
 #include <CommonAPI/CommonAPI.hpp>
 #include <random>
 #include <string>
+#include <fstream>
 #include <thread>
 
 using namespace std::chrono_literals;
 
 constexpr std::string_view DOMAIN = "local";
 constexpr std::string_view INSTANCE = "ldog.gpu.GpuUsageData";
+std::string filePath;
 
 class service_test : public v1::ldog::gpu::GpuUsageDataStubDefault
 {
 private:
-    float randomFloat(float min = 0.0f, float max = 100.0f)
+    std::ifstream fileStream_;
+    int int_from_file()
     {
-        static std::random_device rd;  // Seed
-        static std::mt19937 gen(rd()); // Mersenne Twister RNG
-        std::uniform_real_distribution<float> dist(min, max);
-        return dist(gen);
+        if (!fileStream_.is_open())
+        {
+            std::cerr << "Failed to open file." << std::endl;
+            return 0;
+        }
+
+        int value = 0;
+
+        fileStream_.clear();  // clear EOF flag
+        fileStream_.seekg(0); // rewind file
+
+        if (!(fileStream_ >> value))
+        {
+            std::cerr << "Failed to read value." << std::endl;
+            return 0;
+        }
+
+        return value;
     }
 
-    float getGpuUsage()
+    int getReading()
     {
-        return randomFloat();
+        return int_from_file();
     }
 
 public:
@@ -30,21 +47,34 @@ public:
         const std::shared_ptr<CommonAPI::ClientId> _client,
         requestGpuUsageDataReply_t _reply) override
     {
-        float usage = getGpuUsage(); // your logic
-        std::cout << "Client requested GPU usage: " << usage << "%" << std::endl;
-        _reply(usage); // send response
+        int reading = getReading();
+        std::cout << "Client requested reading: " << reading << std::endl;
+        _reply(reading); // send response
     }
 
-    void broadcastGpuUsageChange()
+    void broadcastGpureadingChange()
     {
-        float usage = getGpuUsage();
-        std::cout << "Broadcasting GPU usage: " << usage << "%" << std::endl;
-        fireNotifyGpuUsageDataChangeEvent(usage);
+        fileStream_.open(filePath);
+        float reading = getReading();
+        std::cout << "Broadcasting reading: " << reading << std::endl;
+        fireNotifyGpuUsageDataChangeEvent(reading);
     }
 };
 
 int main(int argc, char const *argv[])
 {
+
+    if (argc < 2)
+    {
+        filePath = "/sys/bus/iio/devices/iio:device0/in_temp_input";
+    }
+    else
+    {
+        filePath = argv[1];
+    }
+
+    std::cout << "File path: " << filePath << std::endl;
+
     auto runtime = CommonAPI::Runtime::get();
     std::shared_ptr<service_test> myService =
         std::make_shared<service_test>();
@@ -54,7 +84,7 @@ int main(int argc, char const *argv[])
     while (true)
     {
         std::this_thread::sleep_for(100ms);
-        myService->broadcastGpuUsageChange();
+        myService->broadcastGpureadingChange();
         std::cout << "[Server] Still running..." << std::endl;
     }
     return 0;
